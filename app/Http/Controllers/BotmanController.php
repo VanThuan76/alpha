ec354aff9b10743185444fcc199ea9fb2ffd1288
cuongdm <>
2023-08-17 09:32:55 +0700

    update msg txt

--- app/Http/Controllers/BotmanController.php
+++ app/Http/Controllers/BotmanController.php
@@ -48,6 +48,7 @@ class BotmanController extends Controller
                 $msg->info = implode(',', $user->getInfo());
             }
             //$jsonMessages = json_decode($msg->txt, true);
+            $jsonMessages = $msg->txt;
             $jsonMessages[Carbon::now()->format('Y-m-d h:i:s')] = $message;
             $msg->txt = json_encode($jsonMessages);
             $msg->save();
@@ -75,7 +76,8 @@ class OnboardingConversation extends Conversation
                 $msg->last_name = $user->getLastName();
                 $msg->info = implode(',', $user->getInfo());
             }
-            $jsonMessages = json_decode($msg->txt, true);
+            //$jsonMessages = json_decode($msg->txt, true);
+            $jsonMessages = $msg->txt;
             $jsonMessages[Carbon::now()->format('Y-m-d h:i:s')] = $message;
             $msg->txt = json_encode($jsonMessages);
             $msg->phone_number = $message;
