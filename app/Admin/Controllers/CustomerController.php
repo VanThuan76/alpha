20417226658a05c74f3dd17dac1acc43b88137ea
cuongdm <>
2023-08-21 10:26:45 +0700

    add crm

---
+++ app/Admin/Controllers/CustomerController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Customer;
+use App\Models\AdminUser;
+use App\Models\Source;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class CustomerController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Customer';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Customer());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'))->filter('like');
+        $grid->column('address', __('Address'))->filter('like');
+        $grid->column('sale.name', __('Sale'))->filter('like');
+        $grid->column('phone_number', __('Phone number'))->filter('like');
+        $grid->column('telcom', __('Telcom'))->filter('like');
+        $grid->column('source.name', __('Source'))->filter('like');
+        $grid->column('call', __('Call'))->filter('like');
+        $grid->column('sale_note', __('Sale note'))->filter('like')->editable('textarea');
+        $grid->column('next_appointment', __('Next appointment'))->filter('date')->editable('date');
+        $grid->column('status', __('Status'))->using(Constant::STATUS)->filter(Constant::STATUS);
+        $grid->column('created_at', __('Created at'))->vndate();
+        $grid->column('updated_at', __('Updated at'))->vndate();
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Customer::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('address', __('Address'));
+        $show->field('sale', __('Sale'));
+        $show->field('status', __('Status'));
+        $show->field('phone_number', __('Phone number'));
+        $show->field('telcom', __('Telcom'));
+        $show->field('source', __('Source'));
+        $show->field('call', __('Call'));
+        $show->field('sale_note', __('Sale note'));
+        $show->field('next_appointment', __('Next appointment'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Customer());
+
+        $form->text('name', __('Name'))->required();
+        $form->text('address', __('Address'));
+        $form->select('sale_id', __('Sale'))->options(AdminUser::where('active_unit_id', '=', Admin::user()->active_unit_id)->pluck('name', 'id'))->required()->setWidth(2,2);
+        $form->text('phone_number', __('Phone number'))->required();
+        $form->text('telcom', __('Telcom'));
+        $form->select('source_id', __('Source'))->options(Source::pluck('name', 'id'))->required();
+        $form->number('call', __('Call'));
+        $form->textarea('sale_note', __('Sale note'));
+        $form->date('next_appointment', __('Next appointment'));
+        $form->number('status', __('Status'))->default(1);
+
+        return $form;
+    }
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -29,4 +29,5 @@ Route::group([
     $router->resource('room-orders', RoomOrderController::class);
     $router->resource('msgs', MsgController::class);
     $router->resource('roles', RoleController::class);
+    $router->resource('customers', CustomerController::class);
 });
