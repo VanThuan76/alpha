2a33f69716d361087e7b7c3b2b9d0bbdd02d0f02
cuongdm <>
2023-08-29 14:36:41 +0700

    add admin config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Encore\Admin\Config\Config;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Config::load();  // Add this
         Schema::defaultStringLength(191); 
     }
 }
