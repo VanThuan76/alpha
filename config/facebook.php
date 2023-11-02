befb180c90380547b6bfbcfad668d328d4c03a6a
VanThuan76 <thuanvuvan76@gmail.com>
2023-11-02 17:07:02 +0700

    merge-server

---
+++ config/facebook.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'token' => ['facebook' => [
+        'token' => env('FACEBOOK_TOKEN'),
+        'app_secret' => env('FACEBOOK_APP_SECRET'),
+        'verification'=> env('FACEBOOK_VERIFICATION')
+        ] 
+    ]
+];
